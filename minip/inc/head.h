85ed2052bfd11dc0a2e76542421b249960907b1a
sai-mohith <69009143+sai-mohith@users.noreply.github.com>
2020-10-06 21:54:45 +0530

    Create head.h

---
+++ minip/inc/head.h
@@ -0,0 +1,18 @@
+
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+
+int add(int operand1, int operand2);
+
+int subtract(int operand1, int operand2);
+
+int multiply(int operand1, int operand2);
+
+int divide(int operand1, int operand2);
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
